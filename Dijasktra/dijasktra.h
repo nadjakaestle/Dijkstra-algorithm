be9ed944577f0119ef099b3d437c6c73e1014dd3
Nadja Araújo <nadja.karla_@hotmail.com>
2019-07-03 12:19:30 -0300

    Add files via upload

---
+++ Dijasktra/dijasktra.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int tamanhoMatriz( )
+{
+
+}
+
+int lerMatriz ()
+{
+
+
+}
+
+int minHeapify(){
+
+
+}
+
+int dijsktra(){
+
+}
---
+++ Dijasktra/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dijasktra.h"
+
+int main ()
+{
+
+}
