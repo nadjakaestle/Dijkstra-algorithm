b8bc446d736d3c78fa05daeb843d08ae7d680872
Nadja Araújo <nadja.karla_@hotmail.com>
2019-07-04 01:11:46 -0300

    Add files via upload

--- Dijasktra/dijasktra.hpp
+++ Dijasktra/dijasktra.hpp
@@ -1,6 +1,11 @@
 #include <iostream>
-#include <fstream> 
+#include <fstream>
 using namespace std;
+
+typedef struct vertice
+{
+	int *A;
+}Vertice;
 
 
 int tamanhoMatriz(char *arquivo)
@@ -11,10 +16,10 @@ int tamanhoMatriz(char *arquivo)
 	return n;
 }
 
-int popularMatriz( char *arquivo, int n, void *x)
+int popularMatriz(char *arquivo, int n, void *x)
 {
-	// conversão para matriz de inteiros 
-	int (*matriz)[n] = static_cast<int (*)[n]>(x);
+	// conversão para matriz de inteiros
+	int(*matriz)[n] = static_cast<int(*)[n]>(x);
 
 	int l;
 	ifstream file(arquivo);
@@ -27,10 +32,46 @@ int popularMatriz( char *arquivo, int n, void *x)
 			int valor;
 
 			file >> valor;
-				matriz[i][j] = valor;
-				matriz[j][i] = valor;
-
-			
+			matriz[i][j] = valor;
+			matriz[j][i] = valor;
 		}
+	}
+}
+
+int filhoEsquerda(int i)
+{
+	return 2 * i + 1;
+}
+
+int filhoDireita(int i)
+{
+	return 2 * i + 2;
+};
+
+void minHeapify(vertice *arr, int i, int tamanho_heap)
+{
+	int esquerda = filhoEsquerda(i);
+	int direita = filhoDireita(i);
+	int menor;
+	int trocar;
+
+	if ((esquerda <= tamanho_heap) && (arr->A[esquerda] < arr->A[i]))
+	{
+		menor = esquerda;
+	}
+	else
+		menor = i;
+
+	if ((direita <= tamanho_heap) && (arr->A[direita] < arr->A[menor]))
+	{
+		menor = direita;
+	}
+	if (menor != i)
+	{
+		trocar = arr->A[i];
+		arr->A[i] = arr->A[menor];
+		arr->A[menor] = trocar;
+
+		minHeapify(arr, menor, tamanho_heap);
 	}
 }
